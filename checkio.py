5f04625b10e771c07a5e47d2c017aea584ae67ec
Vlad Dmytruk <vladislav.dmitruk777@gmail.com>
2025-01-27 17:43:48 +0200

    main new task in checkio

--- checkio.py
+++ checkio.py
@@ -24,3 +24,31 @@ assert sum_numbers("5 plus 6 is") == 11
 assert sum_numbers("") == 0
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+#-------------------------------#
+
+def checkio(words: str) -> bool:
+    # add your code here
+    lyrics = words.split()
+    count = 0
+    for word in lyrics:
+        if word.isalpha():
+            count += 1
+            if count == 3:
+                return True
+        else:
+            count = 0
+    return False
+
+
+print("Example:")
+print(checkio("Hello World hello"))
+
+# These "asserts" are used for self-checking
+assert checkio("Hello World hello") == True
+assert checkio("He is 123 man") == False
+assert checkio("1 2 3 4") == False
+assert checkio("bla bla bla bla") == True
+assert checkio("Hi") == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
