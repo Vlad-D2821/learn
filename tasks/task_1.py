fd42b3dab7046b1113c7af48491a4fe34a315c99
Vlad Dmytruk <vladislav.dmitruk777@gmail.com>
2025-01-31 10:08:29 +0200

    main task with dict

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -2,3 +2,13 @@
 text_before = 'bondsOutstanding'
 text_before2 = text_before[:1].upper() + text_before[1:]
 print(text_before2)
+
+#--------------#
+count_chars = ("hello")
+char_count = {}
+for symbol in count_chars:
+    if symbol in char_count:
+        char_count[symbol] += 1
+    else:
+        char_count[symbol] = 1
+print(char_count)
